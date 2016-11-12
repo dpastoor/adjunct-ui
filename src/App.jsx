94c20ce4a78c7499382c0fd76b42839f3df5b2aa
Devin Pastoor <devin.pastoor@gmail.com>
2016-11-12 17:07:55 -0500

    add material ui and types

--- src/App.jsx
+++ src/App.jsx
@@ -7,32 +7,17 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 import * as React from 'react';
 import './App.css';
 import { observer } from 'mobx-react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 let App = class App extends React.Component {
     constructor(props) {
         super(props);
-        let s = {
-            counterValue: 0
-        };
-        this.state = s;
-        this.increment = this.increment.bind(this);
-    }
-    increment() {
-        this.setState({
-            counterValue: this.state.counterValue + 1
-        });
     }
     render() {
-        return (<div className="App">
-        <div className="App-header">
-          <h2>Welcome to React I am excited to be here </h2>
+        return (<MuiThemeProvider>
+        <div className="App">
+           Hello 
         </div>
-        <button onClick={this.increment}>
-        {this.state.counterValue}
-        </button>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>);
+      </MuiThemeProvider>);
     }
 };
 App = __decorate([
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import './App.css';
 import {observer} from 'mobx-react';
-
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 interface Props {}
 
 
@@ -9,33 +9,15 @@ interface Props {}
 class App extends React.Component<Props, {}> {
   constructor(props: Props) {
     super(props)
-    let s: {counterValue: number} = {
-      counterValue: 0
-    }
-    this.state = s;
-    this.increment = this.increment.bind(this)
   }
 
-  increment(): void {
-    this.setState({
-        counterValue: this.state.counterValue+1
-    })
-  }  
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React I am excited to be here </h2>
+      <MuiThemeProvider>
+        <div className="App">
+           Hello 
         </div>
-        <button 
-        onClick={this.increment}
-        >
-        {this.state.counterValue}
-        </button>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </MuiThemeProvider>
     );
   }
 }
