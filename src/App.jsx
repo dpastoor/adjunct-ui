2b0bba8fc2313a088eb539ec90b6cf45f6110da1
Devin Pastoor <devin.pastoor@gmail.com>
2016-11-12 16:54:45 -0500

    remove cruft and get ready to turn into real app

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,27 @@ import * as React from 'react';
 import './App.css';
 import { observer } from 'mobx-react';
 let App = class App extends React.Component {
+    constructor(props) {
+        super(props);
+        let s = {
+            counterValue: 0
+        };
+        this.state = s;
+        this.increment = this.increment.bind(this);
+    }
+    increment() {
+        this.setState({
+            counterValue: this.state.counterValue + 1
+        });
+    }
     render() {
         return (<div className="App">
         <div className="App-header">
           <h2>Welcome to React I am excited to be here </h2>
         </div>
+        <button onClick={this.increment}>
+        {this.state.counterValue}
+        </button>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,34 @@ import {observer} from 'mobx-react';
 
 interface Props {}
 
+
 @observer
 class App extends React.Component<Props, {}> {
+  constructor(props: Props) {
+    super(props)
+    let s: {counterValue: number} = {
+      counterValue: 0
+    }
+    this.state = s;
+    this.increment = this.increment.bind(this)
+  }
 
+  increment(): void {
+    this.setState({
+        counterValue: this.state.counterValue+1
+    })
+  }  
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <h2>Welcome to React I am excited to be here </h2>
         </div>
+        <button 
+        onClick={this.increment}
+        >
+        {this.state.counterValue}
+        </button>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
