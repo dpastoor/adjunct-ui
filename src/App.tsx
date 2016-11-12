7e6b60f4c7f7a6f6160a0e632456cfa41ea3c77d
Devin Pastoor <devin.pastoor@gmail.com>
2016-11-12 11:29:33 -0500

    try to incorporate typescript basically

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import './App.css';
+
+interface Props {}
+
+class App extends React.Component<Props, {}> {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
