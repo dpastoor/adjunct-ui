5881fa2ed04f50fe9c57b417b656d9c19bfedd32
Devin Pastoor <devin.pastoor@gmail.com>
2016-11-12 12:01:26 -0500

    full ts through babel conversion

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
 import * as React from 'react';
 import './App.css';
-class App extends React.Component {
+import { observer } from 'mobx-react';
+let App = class App extends React.Component {
     render() {
         return (<div className="App">
         <div className="App-header">
-          <h2>Welcome to React</h2>
+          <h2>Welcome to React I am excited to be here </h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>);
     }
-}
+};
+App = __decorate([
+    observer
+], App);
 export default App;
 //# sourceMappingURL=App.jsx.map
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import * as React from 'react';
 import './App.css';
+import {observer} from 'mobx-react';
 
 interface Props {}
 
+@observer
 class App extends React.Component<Props, {}> {
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <h2>Welcome to React</h2>
+          <h2>Welcome to React I am excited to be here </h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
