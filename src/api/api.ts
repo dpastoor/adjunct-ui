8e14a9eef3a29938028cdb55ec432d3c2cf1de5c
Devin Pastoor <devin.pastoor@gmail.com>
2016-11-15 12:46:15 -0500

    remove extra console logs

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,8 +1,6 @@
 import * as axios from 'axios'
 export class Api {
     public static runCode(code: string[]): Axios.IPromise<Axios.AxiosXHR<{}>>  {
-        console.log(code)
-        console.log(JSON.stringify(code))
            return axios.post('/api/v1/',
                {
                    session_name: "tmp123",
