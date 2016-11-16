88d25d41d866bc42ea9229e6c3b28a12cd1a9bdb
Devin Pastoor <devin.pastoor@gmail.com>
2016-11-16 18:06:57 -0500

    code should be a single string not an array of strings

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,9 +1,9 @@
 import * as axios from 'axios'
 export class Api {
-    public static runCode(code: string[]): Axios.IPromise<Axios.AxiosXHR<{}>>  {
+    public static runCode(code: string): Axios.IPromise<Axios.AxiosXHR<{}>>  {
            return axios.post('/api/v1/',
                {
-                   session_name: "tmp123",
+                   session_name: "tmp1234",
                    code: code
                },
                {
